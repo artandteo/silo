d7601329d3efda86be9ada3c79f51e4fd92e0201
kevincadieu <kevin.cadieu@gmail.com>
2017-02-03 15:26:39 +0100

    ajout connexion temporaire

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.artandteo.com',
@@ -95,4 +97,6 @@ Rails.application.configure do
       openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
       enable_starttls_auto: true  
   }
+
+
 end
