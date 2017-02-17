28bc99f494872ea9fabee271c4113384f555e873
kevincadieu <kevin.cadieu@gmail.com>
2017-02-17 17:02:21 +0100

    add limit of file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :confirmable
 
-  validates :nom, :email, :password, :password_confirmation, presence: { message: " doit être rempli !" }, on: :registration
-  validates :password, length: { minimum: 6, message: " doit être plus grand (6 caratères minimum)" }, on: :registration
-  validates :nom, uniqueness: { message: " est déjà pris !" }, on: :registration
-  validates_confirmation_of :password, on: :registration
+  validates :password, length: { minimum: 6, message: " doit être plus grand (6 caratères minimum)" }, on: :update
+  validates_confirmation_of :password, on: :update
 
   validates :identifiant_eleve, uniqueness: true, presence: true, on: :desk_add
 
