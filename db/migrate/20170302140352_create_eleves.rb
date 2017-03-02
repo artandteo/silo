10055069f393c441593bd329ceef8d380452d82f
Fred Reuby <frederic.reubrecht@gmail.com>
2017-03-02 17:01:48 +0100

    choix police visuel

--- db/migrate/20170302140352_create_eleves.rb
+++
@@ -1,8 +0,0 @@
-class CreateEleves < ActiveRecord::Migration[5.0]
-  def change
-    create_table :eleves do |t|
-      t.string :identifiant
-      t.string :password
-    end
-  end
-end
--- db/migrate/20170302140453_add_compte_references_to_eleves.rb
+++
@@ -1,5 +0,0 @@
-class AddCompteReferencesToEleves < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :eleves, :compte, foreign_key: true
-  end
-end
