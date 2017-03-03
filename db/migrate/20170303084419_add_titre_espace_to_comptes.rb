90f04520722c1556a9fcf7ebae49dd90bbd3770b
Kévin Cadieu <kevin.cadieu@gmail.com>
2017-03-03 11:46:48 +0100

    fix

--- db/migrate/20170303084419_add_titre_espace_to_comptes.rb
+++ db/migrate/20170303084419_add_titre_espace_to_comptes.rb
@@ -1,5 +1,5 @@
 class AddTitreEspaceToComptes < ActiveRecord::Migration[5.0]
   def change
-    add_column :comptes, :titre_espace, :string, default: "Mon Silo"
+    add_column :comptes, :titre_espace, :string
   end
 end
