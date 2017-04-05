780ad51b53e8b605bd7eea0d3ca2ad6417d9cbc4
Fred Reuby <frederic.reubrecht@gmail.com>
2017-04-05 16:11:18 +0200

    deplacement new cookies pour affichage message upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -237,6 +237,7 @@ class ApplicationController < ActionController::Base
       authorized_ext = [".pdf", ".jpg", ".jpeg", ".mp3"]
       params[:fichiers].each do |file|
         if file.size <= 6144000
+          cookies[:uload] = "ok"
           puts "============ TEST FILE SIZE ==============="
           filename = file.original_filename
           directory = "public/folders/#{current_user.nom}/#{params[:draw]}/#{params[:dossier_courant]}/"
@@ -247,7 +248,6 @@ class ApplicationController < ActionController::Base
             if Dir[File.join("public/folders/#{current_user.nom}/#{params[:draw]}/#{params[:dossier_courant]}/", '**', '*')].count != 25
               puts "============ TEST FILE COUNT ==============="
               # flash[:success] = 'Veuillez patienter, fichier en téléchargement...'
-              cookies[:uload] = "ok"
               File.open(path, "wb") { |f| f.write(file.read) }
               cookies.delete :uload
               # Nouveau fichier bdd
