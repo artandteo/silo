e6e7fe11ddc4268215c2c337b75b63d70649f4b9
Fred Reuby <frederic.reubrecht@gmail.com>
2018-01-18 17:39:44 +0100

    adaptation silo à la gestion de projet pas seulement education

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module Silo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    
-
   config.encoding = "utf-8"
 	# config/initializers/locale.rb
 	 
