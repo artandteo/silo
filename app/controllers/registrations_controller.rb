7bb6409f52fd272818d4e4064e6b8255cce7e5f4
Fred Reuby <frederic.reubrecht@gmail.com>
2018-02-06 17:35:02 +0100

    correction bug meme nom user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     if params.include?(:user) 
-    	if !User.exists?(email: params[:user][:email]) && !User.exists?(nom: params[:user][:nom].to_s.gsub(/\s+/, '_')) 
+    	if !User.exists?(email: params[:user][:email]) || !User.exists?(nom: params[:user][:nom].to_s.gsub(/\s+/, '_'))
     		params[:user][:nom] = params[:user][:nom].to_s.gsub(/\s+/, '_')
     		super
     	else 
